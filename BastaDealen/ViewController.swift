0e637cb8f70e1a51eaf8f7b04446ef8303be9def
Emil Laudon <emil.laudn@gmail.com>
2020-01-20 11:24:14 +0100

    Initial Commit

---
+++ BastaDealen/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BastaDealen
+//
+//  Created by Emil Laudon on 2020-01-20.
+//  Copyright © 2020 Emil Laudon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
